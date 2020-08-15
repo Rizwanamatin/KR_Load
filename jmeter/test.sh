f7fa5578827fe7e28fde309eabd8586eab9419c1
Rizwana <shamim@Shamims-MacBook-Pro.local>
2020-08-16 11:02:05 +1200

    Testing with Ultimate thread group

--- jmeter/test.sh
+++ jmeter/test.sh
@@ -10,6 +10,8 @@ echo "Host: $host"
 
 T_DIR=${PWD}
 
+
+
 # Reporting dir: start fresh
 R_DIR=$T_DIR/report
 rm -rf ${R_DIR} > /dev/null 2>&1
@@ -20,7 +22,7 @@ rm -f ${T_DIR}/test/test-plan.jtl ${T_DIR}/test/jmeter.log  > /dev/null 2>&1
 bash run.sh $rootPath -Dlog_level.jmeter=DEBUG \
 	-Jhost=$host \
 	-n -t $T_DIR/test/$testFile -l $T_DIR/test/test-plan.jtl -j $T_DIR/test/jmeter.log \
-	-e -o $R_DIR
+	-e -o $R_DIR --jmeterproperty "jmeter.reportgenerator.exporter.html.series_filter=^(ConfirmationPage|IndexPage|PurchasePage|ReservePage|Test)(-success|-failure)?$"
 
 echo "==== jmeter.log ===="
 cat $T_DIR/test/jmeter.log
